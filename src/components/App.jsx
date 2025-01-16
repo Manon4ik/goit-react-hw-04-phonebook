ce4b09474cfcc79f1e16020ab2651115a4d81449
Misha <manon4ik@gmail.com>
2025-01-16 15:58:09 +0200

    hw 4 phonebook setState done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
 
-export default class App extends Component {
+export default function App() {
 
-  state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-    name: '',
-    number: ''
-  }
+  const [contacts, setContacts] = useState([
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ])
 
-  handleSubmit = evt => {
+  const [filter, setFilter] = useState('')
+  
+  const handleSubmit = evt => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const name = form.name.value;
@@ -26,57 +23,54 @@ export default class App extends Component {
     const nameId = nanoid();
     const person = { name: name, id: nameId, number }
 
-    const exist = this.state.contacts.find(item => item.name === person.name)
+    const exist = contacts.find(item => item.name === person.name)
     
     if (exist) {
+
       alert(`${person.name} already in contacts.`)
+
     } else {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, person]
-      }))
+
+      setContacts([...contacts, person])
+
     }
-    //console.log(this.state);
 
     form.reset()
 
   }
 
-  handleFilter = evt => {
+  const handleFilter = evt => {
     const findValue = evt.currentTarget.value.toLowerCase()
 
-    this.setState({ filter: findValue })
+    setFilter(findValue)
 
   }
 
-  handleDelete = (id) => {
-    //console.log('test:',id);
-    this.setState({contacts: this.state.contacts.filter(item => item.id !== id) })
+  const handleDelete = (id) => {
+    
+    setContacts(contacts.filter(item => item.id !== id))
     
   }
 
-  render() {
 
-    const inputId = nanoid()
-    const telId = nanoid()
-    const filterId = nanoid()
-    const result = this.state.contacts.filter(item => item.name.toLowerCase().includes(this.state.filter))
-    //console.log('result:',result);
+     const inputId = nanoid()
+     const telId = nanoid()
+     const filterId = nanoid()
+     const result = contacts.filter(item => item.name.toLowerCase().includes(filter))
 
-    const contacts = this.state.filter === '' ? this.state.contacts : result
+    const contactsList = filter === '' ? contacts : result
 
-    //console.log('contacts:', contacts);
 
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm inputId={inputId} telId={telId} handleSubmit={this.handleSubmit} />
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm inputId={inputId} telId={telId} handleSubmit={handleSubmit} />
 
-        <h2>Contacts</h2>
-        <Filter filterId={filterId} handleFilter={this.handleFilter} />
+      <h2>Contacts</h2>
+      <Filter filterId={filterId} handleFilter={handleFilter} />
 
-        <ContactList contacts={contacts} handleDelete={this.handleDelete}/>
+      <ContactList contacts={contactsList} handleDelete={handleDelete}/>
 
-      </div>
-    );
-  }
+    </div>
+  );
 }
